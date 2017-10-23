9dde75540ec2ec4abdc162baf623651d80189c66
Jessica Perina <jperina@gmail.com>
2017-10-23 09:41:04 -0700

    test(message.js): add component tests

---
+++ src/__test__/Message.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Message from '../Message';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("Message", () => {
+  let props = {};
+  let mountedMessage;
+  const message = () => {
+    if (!mountedMessage) {
+      mountedMessage = mount(
+          <Message {...props} />
+      );
+    }
+    return mountedMessage;
+  }
+
+  beforeEach(() => {
+    mountedMessage = undefined;
+  });
+  
+  it('always renders a div', () => {
+    const divs = message().find('div');
+    expect(divs.length).toBe(1);
+  });
+
+  describe('inside that div', () => {
+    beforeEach(() => {
+      props.text = 'a test message';
+    })
+
+    it('includes the correct text', () => {
+      const div = message().find('div').first();
+      expect(div.props().children).toEqual('a test message');
+    })
+  })
+});
